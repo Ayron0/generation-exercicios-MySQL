872c0a573de3ba498efa2beb27e0b7a0d446df75
Ayron Paulo de Souza Sant' Anna <ayronpaulo2r@gmail.com>
2025-09-17 12:01:43 -0300

    atividade 1

---
+++ exe01.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE db_RH;
+
+USE db_RH;
+
+CREATE TABLE tb_colaboradores;
+
+CREATE TABLE tb_colaboradores(
+	id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    salario DECIMAL(8,2)  NOT NULL,
+    idade INT,
+    cargo VARCHAR(255) NOT NULL
+);
+
+SELECT * FROM tb_colaboradores;
+
+INSERT INTO tb_colaboradores(nome,  salario, idade, cargo) 
+values ("Paulo", 10000.00, 26, "Presidente");
+
+INSERT INTO tb_colaboradores(nome,  salario, idade, cargo) 
+values ("Pedro", 4000.00, 28, "Diretor");
+
+INSERT INTO tb_colaboradores(nome,  salario, idade, cargo) 
+values ("Cristina" ,7000.00, 65, "Vice Presidenta");
+
+INSERT INTO tb_colaboradores(nome,  salario, idade, cargo) 
+values ("Henrico", 1500.00, 18, "Estagiario");
+
+INSERT INTO tb_colaboradores(nome,  salario, idade, cargo) 
+values ("Julia", 1900.00, 19, "auxiliar adminstrativa");
+
+SELECT * FROM tb_colaboradores;
+
+SELECT * FROM tb_colaboradores WHERE salario < 2000;
+SELECT * FROM tb_colaboradores WHERE salario > 2000;
+
+DROP TABLE tb_colaboradores;
